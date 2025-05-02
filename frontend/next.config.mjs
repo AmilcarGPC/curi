cd587e4d421cb58eb87c64b585b4fb40f4b10687
AmilcarGPC <A18003370@alumnos.uady.mx>
2025-05-01 18:37:39 -0600

    fix: update frontend service configuration to use port 3006

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,7 +11,9 @@ const nextConfig = {
   },
   allowedDevOrigins: [
     'http://localhost:3000',
+    'http://localhost:3006',
     'http://192.168.1.90:3000',
+    'http://209.38.72.198:3006',
   ],
 }
 
